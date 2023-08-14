477cdda7e0e92d366abbd3da995745b1f31a5138
hrkim <hr_0513@naver.com>
2023-08-14 22:16:22 +0900

    add sass

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<template> 
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -6,25 +6,27 @@
   <router-view/>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+  nav {
+    padding: 30px;
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    a {
+      font-weight: bold;
+      color: #2c3e50;
 
-nav a.router-link-exact-active {
-  color: #42b983;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+
+      margin-top: 60px;
+    }
+  }
 }
 </style>

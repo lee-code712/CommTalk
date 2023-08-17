c20f5210383a398ad9eacad18b751831940a23d0
hrkim <hr_0513@naver.com>
2023-08-17 22:07:30 +0900

    add main page

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,32 +1,55 @@
-<template> 
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<template>
+  <div id="main-app">
+    <div class="content-area">
+      <Sidebar/>
+
+      <div class="main-section">
+        <div class="header">
+          <div class="page-name">게시판</div>
+          <gnb>
+              <div class="form-control">
+                  <input type="search" placeholder="search for.."/>
+                  <img src="@/assets/images/fi-rr-search.png"/>
+              </div>
+
+              <div class="icon-wrap">
+                <img src="@/assets/images/fi-rr-envelope.png"/>
+              </div>
+              <div class="icon-wrap">
+                <img src="@/assets/images/fi-rr-user.png"/>
+              </div>
+          </gnb>
+        </div>
+
+        <div class="main-content">
+          <router-view class="router-view" />
+
+          <div class="side-section-wrap">
+            <SideSection/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Sidebar from '@/components/layout/SideBar.vue'
+import SideSection from '@/components/layout/SideSection.vue'
 
-  nav {
-    padding: 30px;
-
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-
-      margin-top: 60px;
-    }
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    SideSection
   }
 }
+</script>
+
+<style lang="scss">
+  @import '@/assets/scss/common.scss';
+</style>
+
+<style scoped lang="scss">
+  @import '@/assets/scss/app.scss';
 </style>
---
+++ src/frontend/src/components/ModalComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="modal-component">
+    <div class="modal-overlay" @click="closeModal">
+      <div class="modal-container" @click.stop>
+        <div class="modal-header">
+          <slot name="header">
+            <span class="modal-title"></span>
+            <span class="modal-close" @click="closeModal">×</span>
+          </slot>
+        </div>
+        <div class="modal-content">
+          <slot></slot>
+        </div>
+        <div class="modal-footer">
+          <slot name="footer">
+            <span class="modal-save" @click="saveModal">저장</span>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalComponent",
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+
+<style scoped>
+  @import "@/assets/scss/modal.scss";
+</style>

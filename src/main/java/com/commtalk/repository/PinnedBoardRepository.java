f5e91e7ebedb09edff6f174209122eee78d24a84
lee-code712 <yurilee07@naver.com>
2023-09-27 16:29:22 +0900

    pinned board 조회코드  수정

--- src/main/java/com/commtalk/repository/PinnedBoardRepository.java
+++ src/main/java/com/commtalk/repository/PinnedBoardRepository.java
@@ -11,7 +11,7 @@ import com.commtalk.model.PinnedBoard;
 @Repository
 public interface PinnedBoardRepository extends JpaRepository<PinnedBoard, Long> {
 
-	@Query("SELECT DISTINCT pb FROM PinnedBoard pb LEFT JOIN FETCH pb.member m JOIN FETCH pb.board b")
-	List<PinnedBoard> findByMemberId(Long memberId);
+	@Query("SELECT DISTINCT pb FROM PinnedBoard pb LEFT JOIN FETCH pb.member m JOIN FETCH pb.board b WHERE m.id = :memberId")
+	List<PinnedBoard> findByMemberId(Long  memberId);
 	
 }

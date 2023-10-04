56e413c1f2cd67f8509ce4cb8167380eed2e183c
leeyuri <yurilee07@naver.com>
2023-10-04 19:13:13 +0900

    에러 수정

--- src/main/java/com/commtalk/repository/PinnedBoardRepository.java
+++ src/main/java/com/commtalk/repository/PinnedBoardRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.commtalk.model.PinnedBoard;
@@ -12,6 +13,6 @@ import com.commtalk.model.PinnedBoard;
 public interface PinnedBoardRepository extends JpaRepository<PinnedBoard, Long> {
 
 	@Query("SELECT DISTINCT pb FROM PinnedBoard pb LEFT JOIN FETCH pb.member m JOIN FETCH pb.board b WHERE m.id = :memberId")
-	List<PinnedBoard> findByMemberId(Long  memberId);
+	List<PinnedBoard> findByMemberId(@Param("memberId") Long memberId);
 	
 }

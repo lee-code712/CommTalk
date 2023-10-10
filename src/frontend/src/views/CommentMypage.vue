d4594a3cc75fd8da44ab13ba2e94da3769e82878
hyeran0513 <hr_0513@naver.com>
2023-10-10 13:03:53 +0900

    내가 쓴 글 / 댓글 단 글 추가

--- src/frontend/src/views/CommentMypage.vue
+++ src/frontend/src/views/CommentMypage.vue
@@ -1,177 +1,139 @@
 <template>
-    <div class="comment-mypage">
-        <MypageSidebar/>
+  <div class="comment-mypage">
+    <MypageSidebar />
 
-        <div class="admin-content-wrap">
-            <MypageHeader/>
+    <div class="admin-content-wrap">
+      <MypageHeader />
 
-            <div class="admin-content">
-                <div class="admin-content-inner">
-                    <strong class="page-title">내가 쓴 글 / 댓글 단 글</strong>
+      <div class="admin-content">
+        <div class="admin-content-inner">
+          <strong class="page-title">내가 쓴 글 / 댓글 단 글</strong>
 
-                    <div class="tabs">
-          <div
-            v-for="(tab, index) in tabs"
-            :key="index"
-            @click="activeTab = tab"
-            :class="{ 'active': activeTab === tab }"
-            class="tab-container"
-          >
-            {{ tab }}
+          <div class="tabs">
+            <div
+              v-for="(tab, index) in tabs"
+              :key="index"
+              @click="activeTab = tab"
+              :class="{ 'active': activeTab === tab }"
+              class="tab-container"
+            >
+              {{ tab }}
+            </div>
+          </div>
+
+          <div class="tab-content">
+            <div v-show="activeTab === '내가 쓴 글'">
+              <ul class="list-wrap">
+                <li v-for="(mycomment, index) in mycomments" :key="index">
+                  <a :href="'/detail?postId=' + mycomment.postId">
+                    <div class="list-content">
+                      <div class="title">{{ mycomment.title }}</div>
+                      <div class="content">{{ mycomment.content }}</div>
+                      <div class="detail-info-wrap">
+                        <div class="active-wrap">
+                          <div>댓글 {{ mycomment.commentCnt }}</div>
+                          <div>공감 {{ mycomment.likes }}</div>
+                          <div>조회수 {{ mycomment.views }}</div>
+                        </div>
+                        <div class="writer-date-wrap">
+                          <div class="date">{{ mycomment.createdAt }}</div>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div v-show="activeTab === '댓글 단 글'">
+              <ul class="list-wrap">
+                <li v-for="(commentPost, index) in commentPosts" :key="index">
+                  <a :href="'/detail?postId=' + commentPost.postId">
+                    <div class="list-content">
+                      <div class="title">{{ commentPost.title }}</div>
+                      <div class="content">{{ commentPost.content }}</div>
+                      <div class="detail-info-wrap">
+                        <div class="active-wrap">
+                          <div>댓글 {{ commentPost.commentCnt }}</div>
+                          <div>공감 {{ commentPost.likes }}</div>
+                          <div>조회수 {{ commentPost.views }}</div>
+                        </div>
+                        <div class="writer-date-wrap">
+                          {{ commentPost.createdAt }}
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-
-        <div class="tab-content">
-          <div v-show="activeTab === '내가 쓴 글'">
-            <div class="top-btns-wrap">
-              <div class="btn-wrap">
-                <label>
-                  <input type="checkbox" class="mycomment-whole-chk" v-model="isAllSelected" @change="toggleSelectAll(mycomments)" />
-                </label>
-                <button type="button" @click="deselectAll(mycomments)">선택해제</button>
-                <button type="button" @click="deleteSelected(mycomments)">선택삭제</button>
-              </div>
-
-              <div class="search-wrap">
-                <input type="text" placeholder="내용을 입력하세요." />
-                <img src="@/assets/images/fi-rr-search.png" />
-              </div>
-            </div>
-            <ul class="list-wrap">
-              <li v-for="(mycomment, index) in mycomments" :key="index">
-                <label>
-                  <input type="checkbox" v-model="mycomment.selected" />
-                </label>
-                <div class="list-content">
-                  <div class="title">{{ mycomment.title }}</div>
-                  <div class="content">{{ mycomment.content }}</div>
-                  <div class="detail-info-wrap">
-                    <div class="active-wrap">
-                      <div>댓글 {{ mycomment.commentCnt }}</div>
-                      <div>공감 {{ mycomment.likeCnt }}</div>
-                      <div>조회수 {{ mycomment.viewCnt }}</div>
-                    </div>
-                    <div class="writer-date-wrap">
-                      <div class="date">{{ mycomment.date }}</div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div v-show="activeTab === '댓글 단 글'">
-            <div class="top-btns-wrap">
-              <div class="btn-wrap">
-                <label>
-                  <input type="checkbox" class="whole-chk" v-model="isAllSelected" @change="toggleSelectAll(commentPosts)" />
-                </label>
-                <button type="button" @click="deselectAll(commentPosts)">선택해제</button>
-                <button type="button" @click="deleteSelected(commentPosts)">선택삭제</button>
-              </div>
-              <div class="search-wrap">
-                <input type="text" placeholder="내용을 입력하세요." />
-                <img src="@/assets/images/fi-rr-search.png" />
-              </div>
-            </div>
-            <ul class="list-wrap">
-              <li v-for="(commentPost, index) in commentPosts" :key="index">
-                <label>
-                  <input type="checkbox" v-model="commentPost.selected" />
-                </label>
-                <div class="list-content">
-                  <div class="title">{{ commentPost.title }}</div>
-                  <div class="content">{{ commentPost.content }}</div>
-                  <div class="detail-info-wrap">
-                    <div class="active-wrap">
-                      <div>댓글 {{ commentPost.commentCnt }}</div>
-                      <div>공감 {{ commentPost.likeCnt }}</div>
-                      <div>조회수 {{ commentPost.viewCnt }}</div>
-                    </div>
-                    <div class="writer-date-wrap">
-                      {{ commentPost.writer }} {{ commentPost.date }}
-                    </div>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
       </div>
-                </div>
-            </div>
-        </div>
     </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 import MypageSidebar from '@/components/layout/MypageSidebar.vue';
 import MypageHeader from '@/components/layout/MypageHeader.vue';
-    export default {
-        name: 'ReportAdmin',
-        components: {
-            MypageSidebar,
-            MypageHeader,
-        },
+
+export default {
+  name: 'CommentMypage',
+  components: {
+    MypageSidebar,
+    MypageHeader,
+  },
   data() {
     return {
       tabs: ['내가 쓴 글', '댓글 단 글'],
       activeTab: '내가 쓴 글',
-      mycomments: [
-        { 
-          title: '내가 쓴 글 제목', 
-          content: '내가 쓴 글 내용', 
-          date: '2023-09-01', 
-          commentCnt: '10',
-          likeCnt: '20',
-          viewCnt: '35',
-          selected: false 
-        },
-         { 
-          title: '내가 쓴 글 제목', 
-          content: '내가 쓴 글 내용', 
-          date: '2023-09-01', 
-          commentCnt: '10',
-          likeCnt: '20',
-          viewCnt: '35',
-          selected: false 
-        },
-      ],
-      commentPosts: [
-        {
-          title: '댓글 단 글 제목',
-          content: '댓글 단 글 내용',
-          writer: '작성자',
-          date: '2023-09-01',
-          commentCnt: '10',
-          likeCnt: '14',
-          viewCnt: '24',
-          selected: false 
-        },
-      ],
-      isAllSelected: false,
+      mycomments: [],
+      commentPosts: [],
     };
   },
-    methods: {
-    toggleSelectAll(dataArray) {
-      const isChecked = this.isAllSelected;
-      dataArray.forEach((item) => (item.selected = isChecked));
+  created() {
+    this.setupHeaders();
+    this.getPost();
+    this.getComment();
+  },
+  methods: {
+    setupHeaders() { /* http 요청 헤더를 설정하고 엔드포인트에 대한 인증 토큰을 포함 */
+      const token = localStorage.getItem('token');
+
+      this.link = 'http://' + window.location.host;
+      this.headers = {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      };
     },
-    deselectAll(dataArray) {
-      dataArray.forEach((item) => (item.selected = false));
-      this.isAllSelected = false;
+    getPost() { /* 내가 쓴 글 */
+      axios
+        .get(this.link + '/api/myPage/getPosts/post', { headers: this.headers })
+        .then((response) => {
+          console.log('내가 쓴 글');
+          this.mycomments = response.data;
+          console.log(this.mycomments);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    deleteSelected(dataArray) {
-      const selectedItems = dataArray.filter((item) => item.selected);
-      selectedItems.forEach((item) => {
-        const index = dataArray.indexOf(item);
-        if (index !== -1) {
-          dataArray.splice(index, 1);
-        }
-      });
-      this.deselectAll(dataArray);
+    getComment() { /* 댓글 단 글 */
+      axios
+        .get(this.link + '/api/myPage/getPosts/comment', { headers: this.headers })
+        .then((response) => {
+          console.log('내가 쓴 댓글');
+          this.commentPosts = response.data;
+          console.log(this.commentPosts);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
-       
-    }
+};
 </script>
 
 <style lang="scss">
